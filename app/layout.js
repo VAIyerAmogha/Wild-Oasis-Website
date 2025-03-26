09430eeade37292130b0aff62bbd4b3019cb5364
Amogha V A <amogha.va.iyer@gmail.com>
2025-03-27 01:47:39 +0530

    Filled home page and the about page

--- app/layout.js
+++ app/layout.js
@@ -1,20 +1,36 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import Header from "./_components/Header";
+import Logo from "./_components/Logo";
+import Navigation from "./_components/Navigation";
+import "@/app/_styles/globals.css";
+
+import { Josefin_Sans } from "next/font/google";
+
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+console.log(josefin);
 
 export const metadata = {
-  title: "The Wild Oasis",
+  // title: "The Wild Oasis",
+  title: {
+    template: "%s / The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  description: "Luxurious cabin hotel, in the heart of a dark forest",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          <Logo />
-        </header>
-        <Navigation />
-        <main>{children}</main>
-        <footer>Copyright by The Wild Oasis</footer>
+      <body
+        className={`bg-primary-950 text-primary-100 min-h-screen ${josefin.className} antialiased flex flex-col relative`}
+      >
+        <Header />
+        <div className="flex-1 px-8 py-12">
+          <main className="max-w-7xl mx-auto ">{children}</main>
+        </div>
       </body>
     </html>
   );
