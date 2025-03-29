d87bea585296c804f31bf53f78881e061b439b2b
Amogha V A <amogha.va.iyer@gmail.com>
2025-03-29 14:33:35 +0530

    Implemented DateSelector and Reservation form

---
+++ app/_components/DateSelector.js
@@ -0,0 +1,58 @@
+"use client";
+
+import { isWithinInterval } from "date-fns";
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
+import { useReservation } from "./ReservationContext";
+
+function isAlreadyBooked(range, datesArr) {
+  return (
+    range.from &&
+    range.to &&
+    datesArr.some((date) =>
+      isWithinInterval(date, { start: range.from, end: range.to })
+    )
+  );
+}
+
+function DateSelector({ settings, cabin, bookingDates }) {
+  const { range, setRange, resetRange } = useReservation();
+
+  console.log("Selected range:", range); // Debugging
+
+  const { minBookingLength, maxBookingLength } = settings;
+
+  return (
+    <div className="flex flex-col justify-between">
+      <DayPicker
+        className="pt-12 place-self-center"
+        mode="range"
+        onSelect={(selectedRange) => {
+          // console.log("New range selected:", selectedRange); // Debugging
+          setRange(selectedRange);
+        }}
+        selected={range}
+        fromMonth={new Date()}
+        fromDate={new Date()}
+        toYear={new Date().getFullYear() + 5}
+        captionLayout="dropdown"
+        numberOfMonths={2}
+      />
+
+      <div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
+        <div className="flex items-baseline gap-6">{/* Additional UI */}</div>
+
+        {range.from || range.to ? (
+          <button
+            className="border border-primary-800 py-2 px-4 text-sm font-semibold"
+            onClick={resetRange}
+          >
+            Clear
+          </button>
+        ) : null}
+      </div>
+    </div>
+  );
+}
+
+export default DateSelector;
